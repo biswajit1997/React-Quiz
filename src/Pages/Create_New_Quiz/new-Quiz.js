5c85d05ea7d8c82efb4fec10acd9d821b476532d
Biswajit Pradhan <biswajit.pradhan@nettantra.net>
2023-12-02 12:52:43 +0530

    update

--- src/Pages/Create_New_Quiz/new-Quiz.js
+++ src/Pages/Create_New_Quiz/new-Quiz.js
@@ -3,6 +3,7 @@ import Layout from "../../components/Layout/Layout";
 import { Box, Dialog, DialogActions, DialogTitle } from "@mui/material";
 import Button from "@mui/material/Button";
 import MCQSingle from "./MCQ-Single";
+import CloseIcon from '@mui/icons-material/Close';
 
 const Addquiz = () => {
   const [popup, setPopup] = useState(true);
@@ -11,27 +12,34 @@ const Addquiz = () => {
     setPopup(false);
   };
   return (
-    <Layout>
+    <Layout
+    >
       <Box className="Box1"
-       sx = {{
-        height: '30%',
-        width: '50%',
-      }}
+        sx={{
+          height: '30%',
+          width: '50%',
+          padding:'16px'
+        }}
       >
         {popup ? (
           <Box className="Box2"
-          sx = {{
-            height: '30%',
-            width: '50%',
-          }}
+            sx={{
+              height: '30%',
+              width: '50%',
+             
+            }}
           >
-            <Dialog open={true}
-           
-             aria-labelledby="responsive-dialog-title">
+            <Dialog open={true} aria-labelledby="responsive-dialog-title">
+                <Box component="div" className="lt" sx={{ display: 'flex',justifyContent: 'end', marginTop: '5px' }}>
+                  <CloseIcon />
+                </Box>
               <DialogTitle
                 id="responsive-dialog-title"
-                sx={{ bgcolor: "#fff",
-               }}
+                sx={{
+                  bgcolor: "#fff",
+                  padding:"10px",
+                  width: '600px'
+                }}
               >
                 {"Select Question Type"}
               </DialogTitle>
@@ -41,22 +49,78 @@ const Addquiz = () => {
                   display: "flex",
                   alignItems: "center",
                   justifyContent: "center",
+                  marginBottom: "40px",
+                  display: 'block',
                 }}
               >
-                <Button
-                  sx={{
-                    fontSize: "16px",
-                    marginBottom: "10px",
-                    bgcolor: "#4f7be9",
-                    color: '#fff',
-                    ":hover": { bgcolor: "#4f7be9", color: "#fff" },
-                  }}
-                  onClick={popupfunc}
-                  variant="outlined"
-                  color="error"
-                >
-                  MCQ<p style={{ fontSize: "10px" }}>(Single Correct)</p>
-                </Button>
+
+                <Box component="div" className="lt" sx={{ display: 'inline' }}>
+                  <Button
+                    sx={{
+                      fontSize: "16px",
+                      marginBottom: "10px",
+                      bgcolor: "#4f7be9",
+                      color: '#fff',
+                      ":hover": { bgcolor: "#4f7be9", color: "#fff" },
+                    }}
+                    onClick={popupfunc}
+                    variant="outlined"
+                    color="error"
+                  >
+                    MCQ<p style={{ fontSize: "10px" }}>(Single Correct)</p>
+                  </Button>
+                </Box>
+                <Box component="div" className="lt" sx={{ display: 'inline' }}>
+                  <Button
+                    sx={{
+                      fontSize: "16px",
+                      marginBottom: "10px",
+                      bgcolor: "#4f7be9",
+                      color: '#fff',
+                      ":hover": { bgcolor: "#4f7be9", color: "#fff" },
+                    }}
+                    onClick={popupfunc}
+                    variant="outlined"
+                    color="error"
+                  >
+                    MCQ<p style={{ fontSize: "10px" }}>(Multiple Correct)</p>
+                  </Button>
+
+                </Box>
+                <Box component="div" className="lt"  sx={{ display: 'inline' }}>
+                  <Button
+                    sx={{
+                      fontSize: "16px",
+                      marginBottom: "10px",
+                      bgcolor: "#4f7be9",
+                      color: '#fff',
+                      ":hover": { bgcolor: "#4f7be9", color: "#fff" },
+                    }}
+                    onClick={popupfunc}
+                    variant="outlined"
+                    color="error"
+                  >
+                    Short Answer<p style={{ fontSize: "10px" }}>(1 or 2 Correct)</p>
+                  </Button>
+                </Box>
+                <Box component="div" className="lt" sx={{ display: 'inline' }}>
+                  <Button
+                    sx={{
+                      fontSize: "16px",
+                      marginBottom: "10px",
+                      bgcolor: "#4f7be9",
+                      color: '#fff',
+                      ":hover": { bgcolor: "#4f7be9", color: "#fff" },
+                    }}
+                    onClick={popupfunc}
+                    variant="outlined"
+                    color="error"
+                  >
+                    Description<p style={{ fontSize: "10px" }}>(1 or 2 Correct)</p>
+                  </Button>
+                </Box>
+
+
               </DialogActions>
             </Dialog>
           </Box>
